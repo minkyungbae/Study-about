f5204963648a831a3619c498fb94cc63b83370bd
배민경 <bmk0703@naver.com>
2024-12-07 23:22:15 +0900

    fix 짝수는 싫어요.py

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.py
@@ -5,3 +5,11 @@ def solution(n):
             numbers.append(num)                     # 리스트에 홀수값 넣기
     
     return numbers
+
+
+# range() 함수 쓰면 if 굳이 안 해도 됨
+def solution(n):
+    numbers = []                                    # 리스트 만들기
+    for num in range(1,n+1, 2):                     # range() 함수를 써서 홀수값 찾기
+        numbers.append(num)                         # append() 함수를 써서 리스트에 홀수값 넣어주기
+    return numbers
