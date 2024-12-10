02f608e6e5dd0bf5080ba6ff97a105c35fd087f6
배민경 <bmk0703@naver.com>
2024-12-10 15:15:22 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.py
@@ -0,0 +1,12 @@
+def solution(money):
+    answer = []
+    
+    coffee = money % 5500
+    
+    if coffee == 0:
+        answer = [money // 5500, 0]
+    
+    else:
+        answer = [money // 5500, money % 5500]
+
+    return answer
