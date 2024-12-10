07499a886fbf4c0ef2032d415f1375132d2ff81a
배민경 <bmk0703@naver.com>
2024-12-10 09:40:45 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,16 @@
+import math
+
+def solution(price):
+    if price >= 500000:
+        answer = price - price * 0.2
+        
+    elif price >= 300000:
+        answer = price - price * 0.1
+        
+    elif price >= 100000:
+        answer = price - price * 0.05 
+    
+    else:
+        answer = price
+        
+    return int(math.trunc(answer))
