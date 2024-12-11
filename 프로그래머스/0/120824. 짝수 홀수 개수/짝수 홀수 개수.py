25768bdcddc5dad6eb9de564742e02851bb4e8e5
배민경 <bmk0703@naver.com>
2024-12-11 21:54:17 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    answer = [0,0]
+    
+    for num in num_list:
+        if num % 2 == 0:
+            answer[0] += 1
+            
+        if num % 2 != 0:
+            answer[1] += 1
+            
+    return answer
