46bd054a2daf1fc52ee77305ec72f0b75b57276e
배민경 <bmk0703@naver.com>
2024-12-13 09:13:34 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.py
@@ -0,0 +1,15 @@
+def solution(strlist):
+    total_answer = []
+    # print(answer)
+    # print(len(strlist))
+    
+    for len_answer in strlist:
+        total_answer.append(len(len_answer))
+
+    # for i in range(len(strlist)):
+    #     len_answer = len(strlist[i])
+    #     total_answer.append(len_answer)
+        
+    # print(total_answer)
+    
+    return total_answer
