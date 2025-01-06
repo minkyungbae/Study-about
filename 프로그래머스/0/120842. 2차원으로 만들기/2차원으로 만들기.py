b3db33634d097fcc744a1c150d627527e5aa8c97
배민경 <bmk0703@naver.com>
2025-01-06 17:49:31 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -1,7 +1,12 @@
 def solution(num_list, n):
     answer = []
-    
-    for i in range(0, len(num_list)//n):
-        answer.append(num_list[n*i:(n*i)+n])
-        print(answer)
+
+#     for i in range(0, len(num_list)//n):
+#         answer.append(num_list[n*i:(n*i)+n])
+
+#     return answer
+
+    for i in range(0,len(num_list)//n):
+        answer.append(num_list[n*i:n*(i+1)])
+        
     return answer
--- 프로그래머스/0/120842. 2차원으로 만들기/README.md
+++ 프로그래머스/0/120842. 2차원으로 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 2차원으로 만들기 - 120842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120842?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.03 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 30일 22:43:50
+2025년 01월 06일 17:49:26
 
 ### 문제 설명
 
