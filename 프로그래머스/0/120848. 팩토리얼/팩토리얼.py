b689b27276035338df764ddb8032de85580d7867
배민경 <bmk0703@naver.com>
2025-01-06 18:03:03 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.py
@@ -1,10 +1,18 @@
 def solution(n):
     answer = []
-    fac = 1 # 곱해줘야 하니까 1부터 시작
+#     fac = 1 # 곱해줘야 하니까 1부터 시작
     
-    for i in range(1,11):   # 최대 팩토리얼은 10
-        fac *= i    # 팩토리얼 코드
-        if fac <= n:    # 10! 값보다 작아야 함
-            answer.append(i)
+#     for i in range(1,11):   # 최대 팩토리얼은 10
+#         fac *= i    # 팩토리얼 코드
+#         if fac <= n:    # 10! 값보다 작아야 함
+#             answer.append(i)
         
-    return answer[-1]
+#     return answer[-1]
+
+## 재귀함수
+from math import factorial as fac
+def solution(n):
+    k = 10
+    while n < fac(k):
+        k -= 1
+    return k
