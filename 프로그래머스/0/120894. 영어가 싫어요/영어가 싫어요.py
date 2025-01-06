48c5125605885a2be916e206fd6e3d3eae3afc92
배민경 <bmk0703@naver.com>
2025-01-06 11:55:20 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = 0
+    eng_num = {"zero":"0", "one":"1", "two":"2", "three":"3", "four":"4",
+               "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+    
+    for i in eng_num.keys():
+               numbers = numbers.replace(i,eng_num[i])
+               
+    return int(numbers)
