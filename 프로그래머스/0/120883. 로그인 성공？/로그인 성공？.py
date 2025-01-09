993b50aa51ee6ea69bd62d679c97b04cb38f445f
배민경 <bmk0703@naver.com>
2025-01-09 13:46:26 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,7 +1,7 @@
 def solution(id_pw, db):
     for i in db:
         if id_pw[0] in i:           # 아이디가 db에 있는 경우
-            if id_pw[1] == i[1]:    # 비밀먼호까지 맞을 경우
+            if id_pw[1] == i[1]:    # 비밀번호까지 맞을 경우
                 return "login"
             else:                   # 비밀번호가 다를 경우
                 return "wrong pw"
