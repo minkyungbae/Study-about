2fd7c79e684844b79ed377fb3e46cb360c78ad81
배민경 <bmk0703@naver.com>
2025-02-05 16:06:29 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,6 +1,7 @@
 def solution(hp):
     general_ant_count = hp//5
     soldier_ant_count = (hp - (5*general_ant_count))//3
-    worker_ant_count = (hp - (5*general_ant_count) -(3*soldier_ant_count))//1
+    worker_ant_count = (hp - (5*general_ant_count) - (3*soldier_ant_count))//1
     answer = general_ant_count + soldier_ant_count + worker_ant_count
+    
     return answer
